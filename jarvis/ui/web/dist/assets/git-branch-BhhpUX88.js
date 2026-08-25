17f39eadff5b507ae78369dedf170e957e4791cb
rubenluetke10-beep <226271791+rubenluetke10-beep@users.noreply.github.com>
2026-08-25 11:55:09 +0200

    build(ui): bundle the daily spend ledger and the day report

---
+++ jarvis/ui/web/dist/assets/git-branch-BhhpUX88.js
@@ -0,0 +1,11 @@
+import{b as c}from"./index-D0Zy4SRl.js";/**
+ * @license lucide-react v0.445.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const r=c("Briefcase",[["path",{d:"M16 20V4a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v16",key:"jecpp"}],["rect",{width:"20",height:"14",x:"2",y:"6",rx:"2",key:"i6l2r4"}]]);/**
+ * @license lucide-react v0.445.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const a=c("GitBranch",[["line",{x1:"6",x2:"6",y1:"3",y2:"15",key:"17qcm7"}],["circle",{cx:"18",cy:"6",r:"3",key:"1h7g24"}],["circle",{cx:"6",cy:"18",r:"3",key:"fqmcym"}],["path",{d:"M18 9a9 9 0 0 1-9 9",key:"n2h4wq"}]]);export{r as B,a as G};
